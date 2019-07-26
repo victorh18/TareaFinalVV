e95202b0f8a56eb7864baebdcf5d3c9979f2b675
unknown <victorwmh18@gmail.com>
2019-07-26 19:44:58 -0400

    Añadiendo las pruebas unitarias

---
+++ ModuloGestorNotasTestProject/MateriasTests.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Data;
+using System.Linq;
+using ModuloGestorNotas.Models;
+using ModuloGestorNotas.Controllers;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ModuloGestorNotasTestProject
+{
+    [TestClass]
+    public class MateriasTests
+    {
+        [TestMethod]
+        public void InsertsMaterias()
+        {
+            MateriasController mController = new MateriasController();
+            Materia materia = new Materia() { Nombre = "Psicologia" };
+            mController.Create(materia);
+            ApplicationDbContext db = new ApplicationDbContext();
+            materia = db.Materia.Where(m => m.Nombre == "Psicologia").First();
+            Assert.IsTrue(materia != null);
+            db.Materia.Remove(materia);
+
+            
+        }
+    }
+}
---
+++ ModuloGestorNotasTestProject/PeriodoTests.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Data;
+using System.Linq;
+using ModuloGestorNotas.Controllers;
+using ModuloGestorNotas.Models;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ModuloGestorNotasTestProject
+{
+    [TestClass]
+    public class PeriodoTests
+    {
+        [TestMethod]
+        public void InsertsPeriodos()
+        {
+            PeriodosController controller = new PeriodosController();
+            ApplicationDbContext db = new ApplicationDbContext();
+            Periodo periodoPrueba = new Periodo() { Anio = 2018, Cuatrimestre = 3};
+            controller.Create(periodoPrueba);
+            periodoPrueba = db.Periodo.Where(g => g.Codigo == "2018-C2").First();
+            Assert.IsTrue(periodoPrueba != null);
+            db.Periodo.Remove(periodoPrueba);
+        }
+    }
+}
